cd0bc05eec78f01f748cfb0e8c92d174b2646423
James Scully <james.will.scully@gmail.com>
2020-01-07 21:29:32 +0000

    Added basic API class + passengers argument

---
+++ src/com/scully/ApiResult.java
@@ -0,0 +1,6 @@
+package com.scully;
+
+/** This class acts as a model for the returned json **/
+public class ApiResult {
+
+}
--- src/com/scully/Main.java
+++ src/com/scully/Main.java
@@ -4,28 +4,32 @@ import java.text.ParseException;
 
 public class Main {
 
-    // args format: [pickup_lat] [pickup_lng] [dropoff_lat] [dropoff_lng] (we'll start with this one)
+    // args format: [passengers] [pickup_lat] [pickup_lng] [dropoff_lat] [dropoff_lng] (we'll start with this one)
     // alternatively we could do: pickup[lat,lng] dropoff[lat,lng]
     public static void main(String[] args) {
 
 
         // test that our args are valid
-        if(args.length != 4) {
+        if(args.length != 5) {
             System.err.println("Incorrect number of arguments");
             System.err.println("Argument format: pickup_latitude pickup_longitude dropoff_latitude dropoff_longitude");
             System.exit(1);
         }
 
+        int passengers = 0;
+
         double  pLat = 0.0, pLng = 0.0,
                 dLat = 0.0, dLng = 0.0;
 
         try {
-            pLat = Double.parseDouble(args[0]);
-            pLng = Double.parseDouble(args[1]);
-            dLat = Double.parseDouble(args[2]);
-            dLng = Double.parseDouble(args[3]);
+            passengers = Integer.parseInt(args[0]);
+
+            pLat = Double.parseDouble(args[1]);
+            pLng = Double.parseDouble(args[2]);
+            dLat = Double.parseDouble(args[3]);
+            dLng = Double.parseDouble(args[4]);
         } catch (NumberFormatException e) {
-            System.err.println("Invalid argument passed");
+            System.err.println("Invalid argument(s) passed");
             e.printStackTrace();
             System.exit(1);
         }
@@ -36,6 +40,10 @@ public class Main {
         System.out.println(
                 String.format("Dropoff Location: (%f,%f)", dLat, dLng)
         );
+
+
+
+
 
 
 
