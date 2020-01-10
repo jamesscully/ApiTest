246d24f33fd6b9dfcdc160add42d92ea0bb57b53
James Scully <james.will.scully@gmail.com>
2020-01-10 01:03:03 +0000

    Converted project to gradle

--- src/test/java/com/scully/MainTest.java
+++ src/test/java/com/scully/MainTest.java
@@ -1,7 +1,7 @@
-import com.scully.*;
-import org.junit.Test;
+package com.scully;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MainTest {
 
